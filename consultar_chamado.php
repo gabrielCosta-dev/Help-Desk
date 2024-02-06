bf11b8c9e4024859d0047995f5498588ae8fa2ee
Gabriel Lima <lima20gabriel@gmail.com>
2024-02-06 17:40:52 -0300

    refactoring

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,9 +1,5 @@
 <?php 
-  session_start();
-
-  if ($_SESSION['autenticado'] != 'SIM' || !isset($_SESSION['autenticado'])) {
-    header('Location:http://localhost/_App-Help-Desk/index.php?login=erro2');
-  }
+  require_once "validador_acesso.php"
 ?>
 
 <html>
